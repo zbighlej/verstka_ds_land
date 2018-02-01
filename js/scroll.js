5846971d84bd0378119b94471b1877f483fef0d5
zbighlej <mikeyork32@gmail.com>
2018-02-01 11:12:00 +0200

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	    $("#navblock").on("click","a", function (event) {
+	        event.preventDefault();
+	        var id  = $(this).attr('href'),
+	            top = $(id).offset().top;
+	        $('body,html').animate({scrollTop: top}, 25000);
+
+	    });
+
+	});
